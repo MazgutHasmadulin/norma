326b90b32f947e30f872e66e36a3adc7e8a5dff8
NikitaKasovsky <nikita@kasovsky.ru>
2025-11-12 00:41:46 +0300

    some fixes

--- norma/urls.py
+++ norma/urls.py
@@ -1,17 +1,32 @@
-from django.urls import path, re_path
+from django.urls import path
 from . import views
+
 urlpatterns = [
+    # Проекты
     path('', views.proj_list, name='proj_list'),
-    path('proj/<int:pk>/', views.proj_dashboard, name='proj_dashboard'),
     path('proj/new/', views.proj_new, name='proj_new'),
+    path('proj/<int:pk>/', views.proj_dashboard, name='proj_dashboard'),
     path('proj/<int:pk>/edit/', views.proj_edit, name='proj_edit'),
     path('proj/<int:pk>/delete/', views.proj_delete, name='proj_delete'),
-    path('folders/new', views.folder_new, name='folder_new'),
-    path('folders/int:pk/edit', views.folder_edit, name='folder_edit'),
-    path('folders/int:pk/delete', views.folder_delete, name='folder_delete'),
-    path('cases/new', views.case_new, name='case_new' ),
-    path('cases/<int:pk>/edit/', views.case_edit, name='case_edit' ),
-    path('cases/<int:pk>/delete/', views.case_delete, name='case_delete' ),
-    path('cases/<int:pk>',views.case_detail, name='case_detail'),
+
+    # Папки (древовидная вложенность)
+    path('proj/<int:proj_pk>/folder/new/', views.folder_new, name='folder_new'),
+    path('proj/<int:proj_pk>/folder/<int:folder_pk>/', views.folder_detail, name='folder_detail'),
+    path('proj/<int:proj_pk>/folder/<int:folder_pk>/edit/', views.folder_edit, name='folder_edit'),
+    path('proj/<int:proj_pk>/folder/<int:folder_pk>/delete/', views.folder_delete, name='folder_delete'),
+
+    # Кейсы (в корне проекта или в папке любого уровня)
+    path('proj/<int:proj_pk>/case/new/', views.case_new, name='case_new'),
+    path('proj/<int:proj_pk>/case/<int:case_pk>/', views.case_detail, name='case_detail'),
+    path('proj/<int:proj_pk>/case/<int:case_pk>/edit/', views.case_edit, name='case_edit'),
+    path('proj/<int:proj_pk>/case/<int:case_pk>/delete/', views.case_delete, name='case_delete'),
+
+    # Кейсы внутри папки
+    path('proj/<int:proj_pk>/folder/<int:folder_pk>/case/new/', views.case_new, name='case_new_in_folder'),
+    path('proj/<int:proj_pk>/folder/<int:folder_pk>/case/<int:case_pk>/', views.case_detail, name='case_detail_in_folder'),
+    path('proj/<int:proj_pk>/folder/<int:folder_pk>/case/<int:case_pk>/edit/', views.case_edit, name='case_edit_in_folder'),
+    path('proj/<int:proj_pk>/folder/<int:folder_pk>/case/<int:case_pk>/delete/', views.case_delete, name='case_delete_in_folder'),
+
+    # Список кейсов проекта (корень)
     path('proj/<int:pk>/cases/', views.cases_list, name='cases_list'),
 ]
--- norma/views.py
+++ norma/views.py
@@ -1,20 +1,22 @@
+
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Proj, Folders, Cases, Launches, TestRunResult
 from .forms import ProjCreationForm, CaseCreationForm, FolderCreationForm
 from django.db.models import Prefetch
 
+# --- Проекты ---
 def proj_list(request):
     projs = Proj.objects.filter(last_update_date__lte=timezone.now()).order_by('last_update_date')
     return render(request, 'norma/proj_list.html', {'projs':projs})
 
 def proj_dashboard(request, pk):
-    proj=Proj.objects.get(pk=pk)
+    proj = get_object_or_404(Proj, pk=pk)
     return render(request, 'norma/proj_dashboard.html', {'proj': proj})
 
 def proj_new(request):
     if request.method == "POST":
-        form=ProjCreationForm(request.POST)
+        form = ProjCreationForm(request.POST)
         if form.is_valid():
             proj = form.save(commit=False)
             proj.author = request.user
@@ -23,7 +25,7 @@ def proj_new(request):
             return redirect('proj_dashboard', pk=proj.pk)
     else:
         form = ProjCreationForm()
-    return render(request, 'norma/proj_edit.html',{'form':form})
+    return render(request, 'norma/proj_edit.html', {'form': form})
 
 def proj_edit(request, pk):
     proj = get_object_or_404(Proj, pk=pk)
@@ -40,57 +42,94 @@ def proj_edit(request, pk):
     return render(request, 'norma/proj_edit.html', {'form': form})
 
 def proj_delete(request, pk):
-    proj=get_object_or_404(Proj, pk=pk)
+    proj = get_object_or_404(Proj, pk=pk)
     proj.delete()
-    return redirect('norma/base.html')
+    return redirect('proj_list')
 
-def cases_list(request, pk):
-    # Получаем проект по ID
-    project = get_object_or_404(Proj, id=pk)
-    
-    # Получаем все папки проекта с предзагрузкой связанных тест-кейсов
-    folders = Folders.objects.filter(project=project).prefetch_related(
-        Prefetch(
-            'cases_folder',
-            queryset=Cases.objects.all().order_by('title')
-        )
-    ).order_by('name')
-    
-    # Получаем тест-кейсы, которые не находятся в папках (если такие есть)
-    cases_without_folder = Cases.objects.filter(
-        folder__project=project, 
-        folder__isnull=True
-    ).order_by('title')
-    
-    context = {
+# --- Папки (древовидная структура) ---
+def folder_detail(request, proj_pk, folder_pk):
+    project = get_object_or_404(Proj, pk=proj_pk)
+    folder = get_object_or_404(Folders, pk=folder_pk, project=project)
+    subfolders = folder.subfolders.all().order_by('name')
+    cases = folder.cases_folder.all().order_by('title')
+    return render(request, 'norma/folder_detail.html', {
         'project': project,
-        'folders': folders,
-        'cases_without_folder': cases_without_folder,
-    }
-    
-    return render(request, 'norma/cases_list.html', context)
+        'folder': folder,
+        'subfolders': subfolders,
+        'cases': cases,
+    })
 
-def case_detail(request, pk):
-    """Детальная страница тест-кейса"""
-    case = get_object_or_404(Cases, pk=pk)
-    return render(request, 'norma/case_detail.html', {'case': case})
-
-def case_new(request):
+def folder_new(request, proj_pk, folder_pk=None):
+    project = get_object_or_404(Proj, pk=proj_pk)
+    parent_folder = None
+    if folder_pk:
+        parent_folder = get_object_or_404(Folders, pk=folder_pk, project=project)
     if request.method == "POST":
-        form=CaseCreationForm(request.POST)
+        form = FolderCreationForm(request.POST)
+        if form.is_valid():
+            folder = form.save(commit=False)
+            folder.project = project
+            folder.parent_folder = parent_folder
+            folder.save()
+            return redirect('folder_detail', proj_pk=project.pk, folder_pk=folder.pk)
+    else:
+        form = FolderCreationForm(initial={'project': project, 'parent_folder': parent_folder})
+    return render(request, 'norma/folder_edit.html', {'form': form, 'project': project, 'parent_folder': parent_folder})
+
+def folder_edit(request, proj_pk, folder_pk):
+    project = get_object_or_404(Proj, pk=proj_pk)
+    folder = get_object_or_404(Folders, pk=folder_pk, project=project)
+    if request.method == "POST":
+        form = FolderCreationForm(request.POST, instance=folder)
+        if form.is_valid():
+            folder = form.save(commit=False)
+            folder.project = project
+            folder.save()
+            return redirect('folder_detail', proj_pk=project.pk, folder_pk=folder.pk)
+    else:
+        form = FolderCreationForm(instance=folder)
+    return render(request, 'norma/folder_edit.html', {'form': form, 'project': project, 'folder': folder})
+
+def folder_delete(request, proj_pk, folder_pk):
+    project = get_object_or_404(Proj, pk=proj_pk)
+    folder = get_object_or_404(Folders, pk=folder_pk, project=project)
+    parent_folder = folder.parent_folder
+    folder.delete()
+    if parent_folder:
+        return redirect('folder_detail', proj_pk=project.pk, folder_pk=parent_folder.pk)
+    else:
+        return redirect('proj_dashboard', pk=project.pk)
+
+# --- Кейсы (в корне проекта и в папках) ---
+def case_detail(request, proj_pk, case_pk, folder_pk=None):
+    project = get_object_or_404(Proj, pk=proj_pk)
+    case = get_object_or_404(Cases, pk=case_pk, folder__project=project)
+    return render(request, 'norma/case_detail.html', {'case': case, 'project': project})
+
+def case_new(request, proj_pk, folder_pk=None):
+    project = get_object_or_404(Proj, pk=proj_pk)
+    folder = None
+    if folder_pk:
+        folder = get_object_or_404(Folders, pk=folder_pk, project=project)
+    if request.method == "POST":
+        form = CaseCreationForm(request.POST)
         if form.is_valid():
             case = form.save(commit=False)
             case.author = request.user
             case.published_date = timezone.now()
+            case.folder = folder
             case.save()
-            proj=case.folder.project.id
-            return redirect('cases_list', pk=proj)
+            if folder:
+                return redirect('folder_detail', proj_pk=project.pk, folder_pk=folder.pk)
+            else:
+                return redirect('proj_dashboard', pk=project.pk)
     else:
-        form = CaseCreationForm()
-    return render(request, 'norma/proj_edit.html',{'form':form})#поменять, мы хотим возвращаться в список кейсов
+        form = CaseCreationForm(initial={'author': request.user, 'folder': folder})
+    return render(request, 'norma/case_edit.html', {'form': form, 'project': project, 'folder': folder})
 
-def case_edit(request, pk):
-    case = get_object_or_404(Cases, pk=pk)
+def case_edit(request, proj_pk, case_pk, folder_pk=None):
+    project = get_object_or_404(Proj, pk=proj_pk)
+    case = get_object_or_404(Cases, pk=case_pk, folder__project=project)
     if request.method == "POST":
         form = CaseCreationForm(request.POST, instance=case)
         if form.is_valid():
@@ -98,45 +137,35 @@ def case_edit(request, pk):
             case.author = request.user
             case.published_date = timezone.now()
             case.save()
-            proj=case.folder.project.id
-            return redirect('cases_list', pk=proj)
+            folder = case.folder
+            if folder:
+                return redirect('folder_detail', proj_pk=project.pk, folder_pk=folder.pk)
+            else:
+                return redirect('proj_dashboard', pk=project.pk)
     else:
         form = CaseCreationForm(instance=case)
-    return render(request, 'norma/proj_edit.html', {'form': form})#поменять, мы хотим возвращаться в список кейсов
+    return render(request, 'norma/case_edit.html', {'form': form, 'project': project, 'case': case})
 
-def case_delete(request, pk):
-    case=get_object_or_404(Cases, pk=pk)
-    proj=case.folder.project.id
+def case_delete(request, proj_pk, case_pk, folder_pk=None):
+    project = get_object_or_404(Proj, pk=proj_pk)
+    case = get_object_or_404(Cases, pk=case_pk, folder__project=project)
+    folder = case.folder
     case.delete()
-    return redirect('cases_list', pk=proj)
-
-def folder_new(request):
-    if request.method == "POST":
-        form=FolderCreationForm(request.POST)
-        if form.is_valid():
-            folder = form.save(commit=False)
-            folder.save()
-            proj=folder.project.id
-            return redirect('cases_list', pk=proj)
+    if folder:
+        return redirect('folder_detail', proj_pk=project.pk, folder_pk=folder.pk)
     else:
-        form = FolderCreationForm()
-    return render(request, 'norma/proj_edit.html',{'form':form})#поменять, мы хотим возвращаться в список кейсов
+        return redirect('proj_dashboard', pk=project.pk)
 
-def folder_edit(request, pk):
-    folder = get_object_or_404(Folders, pk=pk)
-    if request.method == "POST":
-        form = FolderCreationForm(request.POST, instance=form)
-        if form.is_valid():
-            folder = form.save(commit=False)
-            folder.save()
-            proj=folder.project.id
-            return redirect('cases_list', pk=proj)
-    else:
-        form = FolderCreationForm(instance=form)
-    return render(request, 'norma/proj_edit.html', {'form': form})#поменять, мы хотим возвращаться в список кейсов
-
-def folder_delete(request, pk):
-    folder=get_object_or_404(Folders, pk=pk)
-    proj=folder.project.id
-    folder.delete()
-    return redirect('cases_list', pk=proj)
+# --- Список кейсов проекта (корень) ---
+def cases_list(request, pk):
+    project = get_object_or_404(Proj, id=pk)
+    folders = Folders.objects.filter(project=project, parent_folder=None).prefetch_related(
+        Prefetch('cases_folder', queryset=Cases.objects.all().order_by('title'))
+    ).order_by('name')
+    cases_without_folder = Cases.objects.filter(folder__project=project, folder__isnull=True).order_by('title')
+    context = {
+        'project': project,
+        'folders': folders,
+        'cases_without_folder': cases_without_folder,
+    }
+    return render(request, 'norma/cases_list.html', context)
